a1781194a00a54fa06fb1d936ef3274b07d0eeea
Khai <khaiit@tybach.com.vn>
2026-01-31 09:06:33 +0700

    fix: correct NestJS module DI (AuthModule import)

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,9 +1,10 @@
 import { Module } from "@nestjs/common";
 import { UserService } from "./user.service";
-
+import { AuthModule } from "../auth/auth.module";
 import { UserController } from "./user.controller";
 
 @Module({
+  imports: [AuthModule],
   controllers: [UserController],
   providers: [UserService],
   exports: [UserService],
